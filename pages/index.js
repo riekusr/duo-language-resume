5b22228eea1f878fb2db63a8d2737b1adcf0cc5c
RR <riekusritskes@gmail.com>
2021-10-21 14:41:13 +0200

    fixed en / nl highlight

--- pages/index.js
+++ pages/index.js
@@ -47,10 +47,25 @@ export default function Home({ lang = 'en' }) {
 								d='M17 17h2a2 2 0 002-2v-4a2 2 0 00-2-2H5a2 2 0 00-2 2v4a2 2 0 002 2h2m2 4h6a2 2 0 002-2v-4a2 2 0 00-2-2H9a2 2 0 00-2 2v4a2 2 0 002 2zm8-12V5a2 2 0 00-2-2H9a2 2 0 00-2 2v4h10z'
 							/>
 						</svg>
+						{/* <svg
+							onClick={(e) => window.print()}
+							xmlns='http://www.w3.org/2000/svg'
+							className='h-6 w-6 cursor-pointer'
+							fill='none'
+							viewBox='0 0 24 24'
+							stroke='currentColor'>
+							<path
+								strokeLinecap='round'
+								strokeLinejoin='round'
+								strokeWidth={2}
+								d='M12 10v6m0 0l-3-3m3 3l3-3m2 8H7a2 2 0 01-2-2V5a2 2 0 012-2h5.586a1 1 0 01.707.293l5.414 5.414a1 1 0 01.293.707V19a2 2 0 01-2 2z'
+							/>
+						</svg> */}
+
 						<Link href='/nl'>
 							<div
 								className={`px-3 py-1 cursor-pointer ${
-									enlang ? 'font-medium' : ''
+									!enlang ? 'font-medium' : ''
 								}`}>
 								nl
 							</div>
