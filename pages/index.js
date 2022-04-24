5d37f0c43f28c3ceb6a381b426314996f846a7ea
Riekus Ritskes <riekus@Riekuss-Air.home>
2022-04-24 11:13:28 +0200

    changed text ejrooy

--- pages/index.js
+++ pages/index.js
@@ -292,7 +292,7 @@ export default function Home({ lang = 'en' }) {
                     ? `
 									`
                     : `
-                    . 
+                    
                     `}
                 </p>
               </section>
